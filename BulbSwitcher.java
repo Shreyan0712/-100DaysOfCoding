62329bb9540633bdc3339a96623f8d56b8fab183
Shreyan Dhar <dharshreyan05@gmail.com>
2025-06-10 23:49:02 +0530

    Add files via upload

---
+++ BulbSwitcher.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class BulbSwitcher {
+    public static void main(String[] args) {
+        bulbSwitch(1);
+    }
+    public static int bulbSwitch(int n) {
+
+        return (int) Math.sqrt(n);
+    }
+}
