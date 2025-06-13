eff0b710d6501c529d58de68bebf86ac35e046cf
Shreyan Dhar <dharshreyan05@gmail.com>
2025-06-13 12:55:10 +0530

    Update IncreasingTripletSubsequence.java

--- IncreasingTripletSubsequence.java
+++ IncreasingTripletSubsequence.java
@@ -1,4 +1,3 @@
-public class IncreasingTripletSubsequence {
     public static void main(String[] args) {
         int [] nums = {2,4,-2,-3};
         increasingTriplet(nums);
@@ -7,20 +6,16 @@ public class IncreasingTripletSubsequence {
     public static boolean increasingTriplet(int[] nums) {
         int numI = Integer.MAX_VALUE;
         int numJ = Integer.MAX_VALUE;
-        int numK = 0;
-
         for(int i : nums){
-            if(i < numI) {
+            if(i <= numI) {
                 numI = i;
-            } else if(i > numI && i < numJ) {
+            } else if(i > numI && i <= numJ) {
                 numJ = i;
             }
             if(i > numJ){
-                System.out.println(" Found Triplet ");
                 return true;
             }
         }
-        System.out.println(" No Triplet Found ");
         return false;
     }
 }
